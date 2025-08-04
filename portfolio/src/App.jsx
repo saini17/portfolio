ca39d3d94719aaeab7f365adcc39695c06aa9f51
Saini17 <sumitsainiji000@gmail.com>
2025-08-04 11:52:59 +0530

    Adding all the code for my portfolio

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,24 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+import { Toaster } from "@/components/ui/toaster";
+import { Moon, Sun } from "lucide-react";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+
+function App() {
+  return (
+    <>
+      <Toaster />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
